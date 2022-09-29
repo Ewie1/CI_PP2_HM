84a2109739e8f55f3fe830c1e3fd9483b4732604
Ewie1 <shephvo12@gmail.com>
2022-09-29 14:28:13 +0000

    Add click eventlisteners to rock, paper, scissors selections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,19 @@
+/**
+ * Wait for DOM to finish load before running game
+ * Call rock, paper, scissor selections and add click eventlistener 
+ */
+
 
 document.addEventListener("DOMContentLoaded", function() {
-    let rock_selection = document.getElementById()
+    let rock_selection = document.getElementById("rock");
+    let paper_selection = document.getElementById("paper");
+    let scissors_selection = document.getElementById("scissors");
+
+    rock_selection.addEventListener("click",function(){
+        alert("You clicked rock")
+    })
+    paper_selection.addEventListener("click",function(){}) 
+    scissors_selection.addEventListener("click",function(){}) 
 })
 
 function runGame(){
@@ -32,5 +45,5 @@ function isWinner(){
 }
 
 function selectionResults(){
-    
+
 }

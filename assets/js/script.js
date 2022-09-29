753a36ffbf4465eea223b5303019a457666d81a5
Ewie1 <shephvo12@gmail.com>
2022-09-29 15:20:12 +0000

    Set computerSelection to select randomly

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,20 +3,24 @@
  * Call rock, paper, scissor selections and add click eventlistener 
  */
 
-
 document.addEventListener("DOMContentLoaded", function() {
     let rock_selection = document.getElementById("rock");
     let paper_selection = document.getElementById("paper");
     let scissors_selection = document.getElementById("scissors");
 
     rock_selection.addEventListener("click",function(){
-        alert("You clicked rock")
+        alert("You clicked rock");
+        runGame("rock");
     })
-    paper_selection.addEventListener("click",function(){}) 
-    scissors_selection.addEventListener("click",function(){}) 
+    paper_selection.addEventListener("click",function(){
+        runGame("paper");
+    }) 
+    scissors_selection.addEventListener("click",function(){
+        runGame("scissor");
+    }) 
 })
 
-function runGame(){
+function runGame(playerSelection){
 
 }
 
@@ -24,9 +28,17 @@ function playerSelection(){
 
 }
 
-function computerSelection(){
+/**
+ * 
+ * set function to add random selctions for computer
+ */
 
+function computerSelection(){
+    let selections = ["rock", "paper", "scissors"];
+    let compSelection = Math.floor(Math.random() * 3);
+    return selections[compSelection];
 }
+
 
 function gameRules(){
 

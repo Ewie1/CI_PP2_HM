78e9108421cd923805526a62e79eb30bf84b7c0e
Ewie1 <shephvo12@gmail.com>
2022-09-29 14:13:46 +0000

    Add id rock, paper, scissors for selection divs and add function structure to script.js file

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    let rock_selection = document.getElementById()
+})
+
+function runGame(){
+
+}
+
+function playerSelection(){
+
+}
+
+function computerSelection(){
+
+}
+
+function gameRules(){
+
+}
+
+function incrementPlayerWin(){
+
+}
+
+function incrementPlayerLose(){
+
+}
+
+function isWinner(){
+
+}
+
+function selectionResults(){
+    
+}

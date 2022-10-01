38b32762d3cbe9503362f143cfd778b31a90539b
Ewie1 <shephvo12@gmail.com>
2022-10-01 16:36:01 +0000

    Delcare variables, add function for computer selection, rename player and computer score result classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,23 @@
 /**
- * Wait for DOM to finish load before running game
+ * Declare all variables
+ */
+let rock_selection = document.getElementById("rock");
+let paper_selection = document.getElementById("paper");
+let scissors_selection = document.getElementById("scissors");
+let results = document.getElementsByClassName("result");
+let playercount = document.getElementsByClassName("player-score");
+let computerCount = document.getElementsByClassName("computer-score");
+let scoreBoard = document.getElementsByClassName("score-count")
+
+
+
+
+/**
  * Call rock, paper, scissor selections and add click eventlistener 
  */
 
 document.addEventListener("DOMContentLoaded", function() {
-    let rock_selection = document.getElementById("rock");
-    let paper_selection = document.getElementById("paper");
-    let scissors_selection = document.getElementById("scissors");
-
     rock_selection.addEventListener("click",function(){
-        alert("You clicked rock");
         runGame("rock");
     })
     paper_selection.addEventListener("click",function(){
@@ -20,24 +28,47 @@ document.addEventListener("DOMContentLoaded", function() {
     }) 
 })
 
-function runGame(playerSelection){
-
-}
-
-function playerSelection(){
-
-}
 
 /**
  * 
  * set function to add random selctions for computer
  */
 
-function computerSelection(){
+ function computerSelection(){
     let selections = ["rock", "paper", "scissors"];
     let compSelection = Math.floor(Math.random() * 3);
     return selections[compSelection];
 }
+
+function runGame(playSelections){
+    console.log("me" + playSelections);
+   // let computer = computerSelection();
+   // let player = playerSelection();
+   // switch(computer + player) {
+     //   case "rockscissors":
+       // case "paperrock":
+     //   case "scissorspaper":
+     //      console.log("Win!");
+     //       break;
+     //   case "scissorsrock":
+     //   case "rockpaper":
+     //   case "paperscissors":
+     //       console.log("Lose!"); 
+     //       break;
+     //   case "scissorsscissors":
+     //   case "rockrock":
+     //   case "paperpaper":
+     //       console.log("Draw!");
+     //       break;                      
+
+    }
+
+
+function playerSelection(){
+
+}
+
+
 
 
 function gameRules(){
